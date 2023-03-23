db90cd398c0ca2f985073278dc44e10f66a534ec
Tony Heng <tony.heng@doctolib.com>
2023-03-23 13:28:31 +0100

    style: footer

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,5 @@
 import { component$, Slot } from "@builder.io/qwik";
+import { LuGithub } from "@qwikest/icons/lucide";
 
 export default component$(() => {
   return (
@@ -6,9 +7,21 @@ export default component$(() => {
       <main>
         <Slot />
       </main>
-      <footer>
-        <a href="https://github.com/TonyHg/qwik-todo" target="_blank">
-          Tony Heng - See on github
+      <footer class="w-full flex gap-2 justify-center items-center mt-4">
+        <a
+          href="http://github.com/TonyHg"
+          target="_blank"
+          class="font-bold hoverable"
+        >
+          Tony Heng
+        </a>
+        -
+        <a
+          href="https://github.com/TonyHg/qwik-todo"
+          target="_blank"
+          class="flex gap-2 items-center hoverable"
+        >
+          See on github <LuGithub />
         </a>
       </footer>
     </>
